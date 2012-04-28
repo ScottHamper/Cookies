36e716618b6e6edb122746dc7b0e6401257cf014
Scott Hamper <Scott.Hamper@gmail.com>
2012-04-27 20:31:08 -0400

    Added more unit tests and modified some originals

--- tests/spec/cookies-spec.js
+++ tests/spec/cookies-spec.js
@@ -10,13 +10,11 @@ describe('Cookies', function () {
     it('exists in the "window" object', function () {
         expect(Cookies).toBeDefined();
     });
-
-    it('contains a boolean property named "enabled"', function () {
-        expect(Cookies.enabled).toBeBoolean();
-    });
     
-    it('contains an empty object property named "defaults"', function () {
-        expect(Cookies.defaults).toEqual({});
+    describe('.enabled', function () {
+        it('returns whether or not the browser has cookies enabled', function () {
+            expect(Cookies.enabled).toBeBoolean();
+        });
     });
     
     var cookieKey = 'cookies-spec.js';
@@ -26,11 +24,71 @@ describe('Cookies', function () {
         });
         
         it('sets a cookie in "document.cookie"', function () {
-            expect(document.cookie.indexOf(cookieKey + '=')).not.toBeLessThan(0);
+            expect(document.cookie).toContain(cookieKey + '=');
+        });
+        
+        it('JSON encodes numbers', function () {
+            Cookies.set(cookieKey, 1);
+            expect(document.cookie).toContain(cookieKey + '=' + escape(JSON.stringify(1)));
+        });
+        
+        it('JSON encodes strings', function () {
+            Cookies.set(cookieKey, '1');
+            expect(document.cookie).toContain(cookieKey + '=' + escape(JSON.stringify('1')));
+        });
+        
+        it('JSON encodes arrays', function () {
+            Cookies.set(cookieKey, [1, 2, 3]);
+            expect(document.cookie).toContain(cookieKey + '=' + escape(JSON.stringify([1, 2, 3])));
+        });
+        
+        it('JSON encodes objects', function () {
+            Cookies.set(cookieKey, { key: 'value' });
+            expect(document.cookie).toContain(cookieKey + '=' + escape(JSON.stringify({ key: 'value' })));
+        });
+        
+        it('JSON encodes dates', function () {
+            var date = new Date();
+            Cookies.set(cookieKey, date);
+            expect(document.cookie).toContain(cookieKey + '=' + escape(JSON.stringify(date)));
+        });
+        
+        // No way to know if a cookie is actually secure unless HTTP is used.
+        if (window.location.protocol === 'http:') {
+            it('sets secure cookies when "options.secure" is true', function () {
+                Cookies.set(cookieKey, 1, { secure: true });
+                expect(document.cookie).not.toContain(cookieKey + '=');
+                Cookies.set(cookieKey, 1, { secure: false });
+            });
+        }
+    });
+    
+    describe('.expire(key [, options])', function () {
+        it('returns the "Cookies" object', function () {
+            expect(Cookies.expire(cookieKey)).toBe(Cookies);
+        });
+        
+        it('removes a cookie from "document.cookie"', function () {
+            expect(document.cookie).not.toContain(cookieKey + '=');
         });
     });
     
     describe('.get(key)', function () {
+        it('is aliased by calling "Cookies" as a function', function () {
+            expect(Cookies.set(cookieKey, 1).get(cookieKey)).toEqual(Cookies(cookieKey));
+        });
+        
+        it('returns the most locally scoped cookie value for a specific key', function () {
+            Cookies.set(cookieKey, 2, { path: '/' });
+            expect(Cookies.get(cookieKey)).toEqual(1);
+            Cookies.expire(cookieKey, { path: '/' });
+        });
+        
+        it('returns "undefined" for cookies that don\'t exist', function () {
+            Cookies.expire(cookieKey);
+            expect(Cookies.get(cookieKey)).toBeUndefined();
+        });
+    
         it('parses JSON encoded numbers', function () {
             expect(Cookies.set(cookieKey, 1).get(cookieKey)).toEqual(1);
         });
@@ -47,20 +105,9 @@ describe('Cookies', function () {
             expect(Cookies.set(cookieKey, { key: 'value' }).get(cookieKey)).toEqual({ key: 'value' });
         });
         
-        it('caches cookies', function () {
+        it('caches cookie values', function () {
             Cookies.set(cookieKey, 1).get(cookieKey);
             expect(Cookies._cache[cookieKey]).toEqual(1);
-        });
-    });
-    
-    describe('.expire(key [, options])', function () {
-        it('returns the "Cookies" object', function () {
-            expect(Cookies.expire(cookieKey)).toBe(Cookies);
-        });
-        
-        it('expires a cookie', function () {
-            expect(Cookies.get(cookieKey)).toBeUndefined();
-            expect(document.cookie.indexOf(cookieKey + '=')).toBeLessThan(0);
         });
     });
 });
